7139966d10d4a20ee85bdf4cd19656607ec8e415
PatriciaBlanquet <92404521+PatriciaBlanquet@users.noreply.github.com>
2022-04-22 08:16:13 -0500

    Add files via upload
    Hello Python World!

---
+++ hello_world.py
@@ -0,0 +1,16 @@
+from email import message
+
+
+print("Hello Python interpreter!")
+print("Hello Python World!")
+print("Hello World this is Nelly =D!")
+message="Hello Python World!"
+print(message)
+
+message="Hello Python Crash Course world!"
+print(message)
+
+mesage="Hello Python Crash Course reader!"
+print(mesage)
+
+
